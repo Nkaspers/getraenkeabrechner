bf2681c55f124dbff9617d349f0e46e02af94916
Nkaspers <n.kaspers@aol.de>
2023-03-10 00:09:54 +0100

    added return to home function, fixed Toolbar bugs

--- app/src/main/java/com/tcblauweiss/getraenkeabrechner/SettingsActivity.java
+++ app/src/main/java/com/tcblauweiss/getraenkeabrechner/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.tcblauweiss.getraenkeabrechner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -41,13 +45,48 @@ public class SettingsActivity extends AppCompatActivity {
                 R.id.nav_items_edit, R.id.nav_password_change, R.id.nav_return_to_main)
                 .setOpenableLayout(drawer)
                 .build();
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_settings);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_content_settings);
         assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        setNavigationDrawerListeners(navigationView);
         drawer.openDrawer(GravityCompat.START);
     }
+
+    public void setNavigationDrawerListeners(NavigationView navigationView){
+        navigationView.getMenu().findItem(R.id.nav_members_import).setOnMenuItemClickListener(view -> {
+            Toast.makeText(SettingsActivity.this,"Mitglieder importieren", Toast.LENGTH_SHORT).show();
+            //TODO: Mitglieder importieren
+            return false;
+        });
+
+        navigationView.getMenu().findItem(R.id.nav_entries_cancel).setOnMenuItemClickListener(view -> {
+            Toast.makeText(SettingsActivity.this,"Buchungen stornieren", Toast.LENGTH_SHORT).show();
+            //TODO: Buchungen stornieren
+            return false;
+        });
+
+        navigationView.getMenu().findItem(R.id.nav_entries_export).setOnMenuItemClickListener(view -> {
+            Toast.makeText(SettingsActivity.this,"Buchungen exportieren", Toast.LENGTH_SHORT).show();
+            //TODO: Buchungen exportieren
+            return false;
+        });
+
+        navigationView.getMenu().findItem(R.id.nav_entries_delete_all).setOnMenuItemClickListener(view -> {
+            Toast.makeText(SettingsActivity.this,"Alle Buchungen löschen", Toast.LENGTH_SHORT).show();
+            //TODO: Alle Buchungen löschen
+            return false;
+        });
+
+        navigationView.getMenu().findItem(R.id.nav_return_to_main).setOnMenuItemClickListener(view -> {
+            Intent intent_launch_mainActivity = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent_launch_mainActivity);
+            return false;
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/tcblauweiss/getraenkeabrechner/ui/entries/AllEntriesFragment.java
+++ app/src/main/java/com/tcblauweiss/getraenkeabrechner/ui/entries/AllEntriesFragment.java
@@ -6,16 +6,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.search.SearchBar;
+import com.tcblauweiss.getraenkeabrechner.R;
 
 
 public class AllEntriesFragment extends Fragment {
-
+    SearchBar searchBar;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        View view = inflater.inflate(R.layout.fragment_all_entries, container, false);
+        searchBar = view.findViewById(R.id.searchbar_all_entries);
+        searchBar.setNavigationIcon(R.drawable.ic_menu);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(searchBar);
+        return view;
     }
 
     @Override
