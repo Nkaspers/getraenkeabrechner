d6677bd30f7be8d954f8becd7caf1536474a1494
kaspersn0 <s6nikasp@uni-bonn.de>
2023-03-09 18:54:42 +0100

    first layout for edit_members fragment

--- app/src/main/java/com/tcblauweiss/getraenkeabrechner/ui/members/EditMembersFragment.java
+++ app/src/main/java/com/tcblauweiss/getraenkeabrechner/ui/members/EditMembersFragment.java
@@ -1,22 +1,79 @@
 package com.tcblauweiss.getraenkeabrechner.ui.members;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 
-public class EditMembersFragment extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.search.SearchBar;
+import com.tcblauweiss.getraenkeabrechner.R;
 
+public class EditMembersFragment extends Fragment {
+    FloatingActionButton addMemberFab;
+    AlertDialog addMemberDialog;
+
+    SearchBar searchBar;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_members, container, false);
+        searchBar = view.findViewById(R.id.searchbar_edit_members);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(searchBar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        addMemberFab = view.findViewById(R.id.fab_edit_members_fragment);
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
 
-        return null;
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
+        addMemberFab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                userInputDialog();
+            }
+        });
+        return view;
     }
+
+    private void userInputDialog() {
+        addMemberDialog = new AlertDialog.Builder(getContext())
+                .setTitle(R.string.add_member_dialog_title)
+                .setPositiveButton(R.string.add_member_dialog_confirm_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //TODO: Mitglied hinzufügen
+                    }
+                })
+                .setNegativeButton(R.string.add_member_dialog_cancel_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        addMemberDialog.hide();
+                    }
+                })
+                .create();
+        addMemberDialog.show();
+    }
+
+
 
     @Override
     public void onDestroyView() {
